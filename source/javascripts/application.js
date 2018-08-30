c8d04555571118853bb1fa77b5c3245e0954397b
clement bacle <clement@officinasante.fr>
2018-08-30 17:16:57 +0200

    call to action

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,21 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+var classes = ['iphone', 'mini', 'ipad', 'macbook', 'imac'];
+var current = 0;
+
+function setClass() {
+  current++;
+  
+  if (current >= classes.length) {
+    current = 0;
+  }
+  
+  var device = document.getElementsByClassName('device')[0];
+  device.setAttribute('class', ['device', classes[current]].join(' '));
+  
+  setTimeout(setClass, 2000);
+};
+
+setTimeout(setClass, 2000);
